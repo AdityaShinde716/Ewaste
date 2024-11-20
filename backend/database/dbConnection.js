58931b85445792dececc039f43699b78f88f6731
Aditya Shinde <164852169+AdityaShinde716@users.noreply.github.com>
2024-11-20 20:17:56 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
     mongoose
-      .connect("mongodb+srv://210160203022rishabh:Aditya@cluster0.b1nm8.mongodb.net/?retryWrites=true", { dbName: "Ewaste"})
+      .connect("add connectionstring of your database", { dbName: "Ewaste"})
       .then(() => {
         console.log("Connected to database!");
       })
